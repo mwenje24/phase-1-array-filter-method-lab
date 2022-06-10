30a1264d26d9316e404f79fb14543bdba7a86854
Mwenje <mwenje24@gmail.com>
2022-06-10 21:53:13 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching =(names, returnDrivers)=>{
+    return names.filter((filterdNames) => 
+    filterdNames.toUpperCase() === returnDrivers.toUpperCase());
+}
+
+const fuzzyMatch = (names, returnDrivers) =>{
+    return names.filter((filterdNames) => 
+    filterdNames.toUpperCase().indexOf(returnDrivers.toUpperCase()) === 0);
+}
+
+const matchName = (names, hometown) =>{
+    return names.filter((filterdNames) => 
+    filterdNames.name ===hometown)
+}
